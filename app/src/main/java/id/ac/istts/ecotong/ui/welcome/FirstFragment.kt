c63071ff0aecc9fda16e2edbe12c7de4983a7450
equerlyn <feliciapangestu.fp@gmail.com>
2024-04-26 23:00:14 +0700

    Init color
    Make welcome page

---
+++ app/src/main/java/id/ac/istts/ecotong/ui/welcome/FirstFragment.kt
@@ -0,0 +1,23 @@
+package id.ac.istts.ecotong.ui.welcome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import id.ac.istts.ecotong.R
+import id.ac.istts.ecotong.databinding.FragmentFirstBinding
+import id.ac.istts.ecotong.ui.base.BaseFragment
+
+class FirstFragment : BaseFragment<FragmentFirstBinding>(FragmentFirstBinding::inflate) {
+//    private val viewModel: BlankViewModel by viewModels()
+    override fun setupUI() {
+    }
+
+    override fun setupListeners() {
+    }
+
+    override fun setupObservers() {
+    }
+}
