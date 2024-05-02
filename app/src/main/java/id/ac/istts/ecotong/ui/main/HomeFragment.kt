26eaddba43a7a35a739008c26a930f6e2e1fc2f9
equerlyn <feliciapangestu.fp@gmail.com>
2024-05-02 16:35:55 +0700

    Minor revision

---
+++ app/src/main/java/id/ac/istts/ecotong/ui/main/HomeFragment.kt
@@ -0,0 +1,26 @@
+package id.ac.istts.ecotong.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.ac.istts.ecotong.R
+import id.ac.istts.ecotong.databinding.FragmentHomeBinding
+import id.ac.istts.ecotong.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    override fun setupUI() {
+        with(binding) {
+            
+        }
+    }
+
+    override fun setupListeners() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setupObservers() {
+        TODO("Not yet implemented")
+    }
+}
