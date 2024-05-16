c21af12648077535c53ed12eeabb98319a83dd59
equerlyn <feliciapangestu.fp@gmail.com>
2024-05-16 12:48:34 +0700

    Bottom Navigation
    not perfect

--- app/src/main/java/id/ac/istts/ecotong/ui/MainActivity.kt
+++ app/src/main/java/id/ac/istts/ecotong/ui/MainActivity.kt
@@ -1,18 +1,58 @@
 package id.ac.istts.ecotong.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import dagger.hilt.android.AndroidEntryPoint
+import id.ac.istts.ecotong.R
 import id.ac.istts.ecotong.databinding.ActivityMainBinding
+import id.ac.istts.ecotong.ui.main.HomeFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    // coba-coba
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var homeFragment: HomeFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // coba-coba
+        homeFragment = HomeFragment()
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        supportFragmentManager.beginTransaction().replace(R.id.container, homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, homeFragment).commit()
+                    true
+                }
+                R.id.search -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, homeFragment).commit()
+                    true
+                }
+                R.id.scan -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, homeFragment).commit()
+                    true
+                }
+                R.id.history -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, homeFragment).commit()
+                    true
+                }
+                R.id.profile -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, homeFragment).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
