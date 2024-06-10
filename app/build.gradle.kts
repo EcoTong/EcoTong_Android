1ecf2583c4980ade923041362ccfbff9b1e71cd0
Christian Gary Trisanto <102272678+sugarygary@users.noreply.github.com>
2024-06-10 08:56:41 +0700

    Login with google, awaiting API update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
@@ -5,7 +8,9 @@ plugins {
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
 }
-
+val localProperties = Properties().apply {
+    load(FileInputStream(rootProject.file("local.properties")))
+}
 android {
     namespace = "id.ac.istts.ecotong"
     compileSdk = 34
@@ -16,9 +21,13 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-        buildConfigField("Boolean", "DEBUG", "true")
         buildConfigField("String", "API_BASE_URL", "\"http://192.168.0.9:3000/\"")
 //        TODO("GANTI DENGAN IP MASING MASING")
+        buildConfigField(
+            "String",
+            "SERVER_CLIENT_ID",
+            "\"${localProperties.getProperty("SERVER_CLIENT_ID")}\""
+        )
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
@@ -45,7 +54,7 @@ android {
 }
 
 dependencies {
-    implementation("kr.co.prnd:readmore-textview:1.0.0")
+    implementation(libs.readmore.textview)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -86,7 +95,10 @@ dependencies {
     implementation(libs.androidx.camera.camera2)
     implementation(libs.camera.lifecycle)
     implementation(libs.camera.view)
-
+    //Credentials Manager
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/id/ac/istts/ecotong/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/id/ac/istts/ecotong/ui/auth/login/LoginFragment.kt
@@ -1,10 +1,24 @@
 package id.ac.istts.ecotong.ui.auth.login
 
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.GetCredentialException
 import androidx.navigation.fragment.findNavController
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import id.ac.istts.ecotong.BuildConfig
 import id.ac.istts.ecotong.databinding.FragmentLoginBinding
 import id.ac.istts.ecotong.ui.base.BaseFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
     override fun setupUI() {
         with(binding) {
 
@@ -19,9 +33,52 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
             btnLogin.setOnClickListener {
                 findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
             }
+            btnLoginGoogle.setOnClickListener {
+                val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+                    .setFilterByAuthorizedAccounts(false)
+                    .setServerClientId(BuildConfig.SERVER_CLIENT_ID)
+                    .build()
+
+                val request: GetCredentialRequest = GetCredentialRequest.Builder()
+                    .addCredentialOption(googleIdOption)
+                    .build()
+                val credentialManager = CredentialManager.create(requireContext())
+                coroutineScope.launch {
+                    try {
+                        val result = credentialManager.getCredential(requireContext(), request)
+                        handleSignIn(result)
+                    } catch (e: GetCredentialException) {
+                        Timber.e("MainActivity", "GetCredentialException", e)
+                    }
+                }
+
+            }
         }
     }
 
     override fun setupObservers() {
+    }
+
+    private fun handleSignIn(result: GetCredentialResponse) {
+        when (val credential = result.credential) {
+            is CustomCredential -> {
+                if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                    try {
+                        val googleIdTokenCredential = GoogleIdTokenCredential
+                            .createFrom(credential.data)
+                        Timber.d(googleIdTokenCredential.idToken)
+                        // Send googleIdTokenCredential to your server for validation and authentication
+                    } catch (e: GoogleIdTokenParsingException) {
+                        Timber.e("Received an invalid google id token response", e)
+                    }
+                } else {
+                    Timber.e("Unexpected type of credential")
+                }
+            }
+
+            else -> {
+                Timber.e("Unexpected type of credential")
+            }
+        }
     }
 }
